317df4a005c64eb9b9dd0e167dc9bc3b98a477df
fengzebing <1390653296@qq.com>
2025-03-26 23:51:34 +0800

    :sparkles: feat: 增加搜索、毛玻璃效果

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,10 +2,30 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import mediumZoom from 'medium-zoom';
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+ //import './style.css'
+import "./style/index.css"; //引入自定义的样式
 import Confetti from './components/Confetti.vue'
 export default {
   extends: DefaultTheme,
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      mediumZoom(".main img", { background: "var(--vp-c-bg)" }); 
+    }
+    onMounted(() => {
+      initZoom();
+    })
+    watch(
+      () => route.path, 
+      () => {
+      nextTick(() => {
+        initZoom();
+      })
+    })
+  },
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
