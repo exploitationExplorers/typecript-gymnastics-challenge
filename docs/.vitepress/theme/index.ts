7a4d3b3376df8d0a5ced7519cf65a548111c575b
fengzebing <1390653296@qq.com>
2025-03-28 17:47:37 +0800

    :sparkles: 增加评论区功能

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,7 +3,7 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import mediumZoom from 'medium-zoom';
 import { onMounted, watch, nextTick } from 'vue';
-import { useRoute } from 'vitepress';
+import { useRoute, useData } from 'vitepress';
 import 'virtual:group-icons.css' 
 import "./style/index.css"; //引入自定义的样式
 import { NProgress } from 'nprogress-v2/dist/index.js' // 进度条组件
@@ -12,6 +12,7 @@ import Confetti from './components/Confetti.vue'
 import MyLayout from './components/MyLayout.vue';
 import Mouse from './components/Mouse.vue';
 import MouseFollower from './components/MouseFollower.vue';
+import giscusTalk from 'vitepress-plugin-comment-with-giscus'; // 评论插件
 // 不蒜子
 import { inBrowser } from 'vitepress' 
 import busuanzi from 'busuanzi.pure.js'
@@ -27,6 +28,24 @@ export default {
   extends: DefaultTheme,
   setup() {
     const route = useRoute();
+    const { frontmatter } = useData();
+    giscusTalk({
+      repo: 'exploitationExplorers/typecript-gymnastics-challenge', //仓库
+      repoId: 'R_kgDOODkTiw', //仓库ID
+      category: 'General', // 讨论分类
+      categoryId: 'DIC_kwDOODkTi84Coiji', //讨论分类ID
+      mapping: 'pathname',
+      inputPosition: 'bottom',
+      lang: 'zh-CN',
+      }, 
+      {
+        frontmatter, route
+      },
+      //默认值为true，表示已启用，此参数可以忽略；
+      //如果为false，则表示未启用
+      //您可以使用“comment:true”序言在页面上单独启用它
+      true
+    );
     const initZoom = () => {
       // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
       mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
